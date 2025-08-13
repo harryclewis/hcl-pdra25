57d39b7bbdd0d8b7914140c205ac642ec8106a5d
harryclewis <harrycurrelllewis@gmail.com>
2025-08-13 15:46:23 +0100

    Add function to extract time series from CDF

--- hcl_pdra25/data.py
+++ hcl_pdra25/data.py
@@ -4,6 +4,9 @@ hcl_pdra25: data.py
 Contains useful data functions.
 """
 
+import cdflib
+import numpy as np
+import pandas as pd
 from speasy import amda
 
 
@@ -20,3 +23,18 @@ class CustomVariable:
     def __init__(self, time, data):
         self.time = time
         self.values = data
+
+
+""" Function to extract and trim data from a cdf file"""
+def extract_from_CDF(filename, varname, t_s, t_f):
+
+    # open the CDF file
+    cdf_file = cdflib.CDF(filename)
+
+    # extract the time and values
+    t = pd.to_datetime(cdf_file.varget('TIME'), unit='s').to_numpy()
+    v = cdf_file.varget(varname)
+
+    # trim to desired range and return
+    idx = np.argwhere(np.where((t >= pd.to_datetime(t_s).to_numpy()) & (t <= pd.to_datetime(t_f).to_numpy()), True, False))
+    return CustomVariable(np.squeeze(t[idx]), np.squeeze(v[idx]))
