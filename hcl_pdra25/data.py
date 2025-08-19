d1dd87cc811637337845712f618fc72b5bd2a8b5
harryclewis <harrycurrelllewis@gmail.com>
2025-08-19 11:36:46 +0100

    Routine to extract tplot file into numpy files

--- hcl_pdra25/data.py
+++ hcl_pdra25/data.py
@@ -4,14 +4,14 @@ hcl_pdra25: data.py
 Contains useful data functions.
 """
 
-import cdflib
+
 import numpy as np
-import pandas as pd
-from speasy import amda
 
 
 """ Wrapper around speasy.amda.get_parameter to fill NaNs """
 def get_parameter(name, ts, tf):
+    from speasy import amda
+
     param = amda.get_parameter(name, ts, tf)
     if not param is None:
         param.replace_fillval_by_nan(inplace=True)
@@ -27,6 +27,9 @@ class CustomVariable:
 
 """ Function to extract and trim data from a cdf file"""
 def extract_from_CDF(filename, varname, t_s, t_f):
+    import cdflib
+    import pandas as pd
+    from speasy import amda
 
     # open the CDF file
     cdf_file = cdflib.CDF(filename)
@@ -38,3 +41,23 @@ def extract_from_CDF(filename, varname, t_s, t_f):
     # trim to desired range and return
     idx = np.argwhere(np.where((t >= pd.to_datetime(t_s).to_numpy()) & (t <= pd.to_datetime(t_f).to_numpy()), True, False))
     return CustomVariable(np.squeeze(t[idx]), np.squeeze(v[idx]))
+
+
+"""Function to convert .tplot file into set of .npy files"""
+def tplot_to_npy(filename: str = '', vars: list = [], save_dir: str = ''):
+    from pyspedas import tplot_restore, get_data
+    
+    assert not filename == '', "'filename' argument must be a string e.g. 'data/var.tplot'"
+    assert not vars == [], "Provide a list of tplot variable names to extract"
+    assert not save_dir == '', "'save_dir' argument must be a directory path string e.g. 'data/npy_output/'"
+    
+    # load from the .tplot file into memory
+    tplot_restore(filename)
+
+    # loop through and save variables
+    for var in vars:
+        xr = get_data(var, xarray=True)
+        t = xr.time.values
+        d = xr.values
+        np.save(save_dir+var+'_t', t, allow_pickle=False)
+        np.save(save_dir+var, d, allow_pickle=False)
